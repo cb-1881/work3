5af0a0f8a4106ea15f7dbb02c4f8105c47b5f16d
cole <36876441+cb-1881@users.noreply.github.com>
2024-11-15 16:51:08 -0800

    Delete vmmul-omp-harness-instructional directory

--- vmmul-omp-harness-instructional/build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
-  /Users/nuns/Desktop/harness-mips-/vmmul-omp-harness-instructional/dgemv-basic.cpp
--- vmmul-omp-harness-instructional/build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
-  /Users/nuns/Desktop/harness-mips-/vmmul-omp-harness-instructional/dgemv-vectorized.cpp
